4b0f38747e81b15083dce41d7fe2ff30ffcc5665
Pawlo98 <pawlos1998s@o2.pl>
2022-03-21 12:32:53 +0100

    Test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ import java.math.RoundingMode;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("CPM CALCULATOR");
+        System.out.println("CALCULATOR CPM and BMI");
 
         pause();
 
