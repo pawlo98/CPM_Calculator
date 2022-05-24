1fea1e6583477a521d604c9725b3271cb233d414
Pawlo98 <pawlos1998s@o2.pl>
2022-05-24 16:21:02 +0200

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,3 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-
 public class Main {
     public static void main(String[] args) {
 
